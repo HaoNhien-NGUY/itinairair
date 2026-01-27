0f19eea25cc58a80220ac7fa9a17101194003baa
haonhien <haonhien.nguy@gmail.com>
2026-01-27 01:57:40 +0100

    refact: changed demo link to form

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -8,14 +8,16 @@ use App\Service\MailerService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class DemoController extends AbstractController
 {
-    #[Route('/demo/start', name: 'app_demo_start')]
+    #[Route('/demo/start', name: 'app_demo_start', methods: ['POST'])]
     public function start(
+        Request                $request,
         EntityManagerInterface $entityManager,
         DemoGeneratorService   $demoGenerator,
         Security               $security,
@@ -24,6 +26,10 @@ final class DemoController extends AbstractController
     ): Response {
         if($this->getUser()) {
             return $this->redirectToRoute('app_trip');
+        }
+
+        if (!$this->isCsrfTokenValid('start_demo', $request->request->get('_token'))) {
+            return $this->redirectToRoute('app_home');
         }
 
         $user = (new User())

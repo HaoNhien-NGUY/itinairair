096ffade441a0bb953e8f79cedbdc67231793cb3
haonhien <haonhien.nguy@gmail.com>
2026-02-17 02:13:25 +0100

    test: completed trip service test

--- tests/Service/TripServiceTest.php
+++ tests/Service/TripServiceTest.php
@@ -2,16 +2,21 @@
 
 namespace App\Tests\Service;
 
+use App\Entity\Destination;
 use App\Entity\Trip;
 use App\Repository\DestinationRepository;
 use App\Repository\FlightRepository;
 use App\Service\TripService;
 use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class TripServiceTest extends TestCase
 {
+    private EntityManagerInterface&MockObject $entityManager;
+    private DestinationRepository&MockObject $destinationRepository;
+    private FlightRepository&MockObject $flightRepository;
     private TripService $tripService;
     private \DateTime $startDate;
     private \DateTime $endDate;
@@ -19,19 +24,35 @@ class TripServiceTest extends TestCase
 
     protected function setUp(): void
     {
-        $destinationRepository = $this->createMock(DestinationRepository::class);
-        $flightRepository = $this->createMock(FlightRepository::class);
-        $entityManager = $this->createMock(EntityManagerInterface::class);
+        $this->destinationRepository = $this->createMock(DestinationRepository::class);
+        $this->flightRepository = $this->createMock(FlightRepository::class);
+        $this->entityManager = $this->createMock(EntityManagerInterface::class);
 
         $this->startDate = new \DateTime('2024-01-01');
         $this->endDate = new \DateTime('2024-01-05');
         $this->expectedDays = $this->startDate->diff($this->endDate)->days + 1;
 
         $this->tripService = new TripService(
-            $destinationRepository,
-            $flightRepository,
-            $entityManager
+            $this->destinationRepository,
+            $this->flightRepository,
+            $this->entityManager,
         );
+    }
+
+    public function testCreatePersistsAndFlushes(): void
+    {
+        $trip = new Trip();
+
+        $this->entityManager->expects($this->once())
+            ->method('persist')
+            ->with($trip);
+
+        $this->entityManager->expects($this->once())
+            ->method('flush');
+
+        $result = $this->tripService->create($trip);
+
+        $this->assertSame($trip, $result);
     }
 
     #[DataProvider('datesProvider')]
@@ -54,7 +75,7 @@ class TripServiceTest extends TestCase
 
         $days = $trip->getDays();
 
-        $this->assertCount($this->expectedDays + $expectedDaysModifier, $trip->getDays());
+        $this->assertCount($this->expectedDays + $expectedDaysModifier, $days);
     }
 
     public static function datesProvider(): \Generator
@@ -78,6 +99,50 @@ class TripServiceTest extends TestCase
             'startDateModifier' => '-2 days',
             'endDateModifier' => '+5 days',
             'expectedDaysModifier' => +7,
+        ];
+    }
+
+    /**
+     * @param string[] $countries
+     * @param string[] $cities
+     */
+    #[DataProvider('cityAndCountryProvider')]
+    public function testGetTripStatisticsCalculatesCorrectly(array $countries, array $cities): void
+    {
+        $trip = new Trip();
+
+        $trip->setStartDate($this->startDate);
+        $trip->setEndDate($this->endDate);
+        $tripDurationInDays = $trip->getDurationInDays();
+        $this->tripService->addOrRemoveTripDays($trip, $tripDurationInDays);
+
+        $this->destinationRepository->method('findDestinationCountriesByTrip')
+            ->willReturn($countries);
+        $this->destinationRepository->method('findDestinationCitiesByTrip')
+            ->willReturn($cities);
+
+        $stats = $this->tripService->getTripStatistics($trip);
+
+        $this->assertEquals($tripDurationInDays, $stats['duration']);
+        $this->assertEquals(count($countries), $stats['country_count']);
+        $this->assertEquals(count($cities), $stats['city_count']);
+        $this->assertEquals($countries, $stats['countries']);
+        $this->assertEquals($cities, $stats['cities']);
+    }
+
+    public static function cityAndCountryProvider(): \Generator
+    {
+        yield '2 countries and 3 cities' => [
+            'countries' => ['France', 'Espagne'],
+            'cities' => ['Paris', 'Madrid', 'Barcelone'],
+        ];
+        yield '1 country and 3 cities' => [
+            'countries' => ['France'],
+            'cities' => ['Paris', 'Lyon', 'Toulon'],
+        ];
+        yield '0 country and 0 cities' => [
+            'countries' => [],
+            'cities' => [],
         ];
     }
 }
